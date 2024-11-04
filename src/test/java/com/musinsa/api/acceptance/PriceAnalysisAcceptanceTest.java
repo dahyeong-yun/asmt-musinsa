2068d1a6c8f5487b1ec9f252094618b42e0e38c4
danny <gryb809@gmail.com>
2024-11-04 16:22:03 +0900

    test(price): 최저가 추가의 경우 E2E 테스트 추가

--- src/test/java/com/musinsa/api/acceptance/PriceAnalysisAcceptanceTest.java
+++ src/test/java/com/musinsa/api/acceptance/PriceAnalysisAcceptanceTest.java
@@ -21,27 +21,61 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("가격 조회 관련 기능")
 public class PriceAnalysisAcceptanceTest extends AbstractAcceptanceTest {
 
+    private final String 브랜드_A_명 = "A";
+    private final String 브랜드_B_명 = "B";
+    private final String 브랜드_C_명 = "C";
+    private final String 브랜드_D_명 = "D";
+    private final String 브랜드_E_명 = "E";
+    private final String 브랜드_F_명 = "F";
+    private final String 브랜드_G_명 = "G";
+    private final String 브랜드_H_명 = "H";
+    private final String 브랜드_I_명 = "I";
+
+    private String 브랜드_A_ID;
+    private String 브랜드_B_ID;
+    private String 브랜드_C_ID;
+    private String 브랜드_D_ID;
+    private String 브랜드_E_ID;
+    private String 브랜드_F_ID;
+    private String 브랜드_G_ID;
+    private String 브랜드_H_ID;
+    private String 브랜드_I_ID;
+    ExtractableResponse<Response> 브랜드_A;
+    ExtractableResponse<Response> 브랜드_B;
+    ExtractableResponse<Response> 브랜드_C;
+    ExtractableResponse<Response> 브랜드_D;
+    ExtractableResponse<Response> 브랜드_E;
+    ExtractableResponse<Response> 브랜드_F;
+    ExtractableResponse<Response> 브랜드_G;
+    ExtractableResponse<Response> 브랜드_H;
+    ExtractableResponse<Response> 브랜드_I;
+
+    @BeforeEach
+    void setUp() {
+        샘플_데이터_생성();
+    }
+
     private void 샘플_데이터_생성() {
         // 브랜드 생성
-        ExtractableResponse<Response> 브랜드_A = 브랜드_생성_요청("A");
-        ExtractableResponse<Response> 브랜드_B = 브랜드_생성_요청("B");
-        ExtractableResponse<Response> 브랜드_C = 브랜드_생성_요청("C");
-        ExtractableResponse<Response> 브랜드_D = 브랜드_생성_요청("D");
-        ExtractableResponse<Response> 브랜드_E = 브랜드_생성_요청("E");
-        ExtractableResponse<Response> 브랜드_F = 브랜드_생성_요청("F");
-        ExtractableResponse<Response> 브랜드_G = 브랜드_생성_요청("G");
-        ExtractableResponse<Response> 브랜드_H = 브랜드_생성_요청("H");
-        ExtractableResponse<Response> 브랜드_I = 브랜드_생성_요청("I");
+        브랜드_A = 브랜드_생성_요청(브랜드_A_명);
+        브랜드_B = 브랜드_생성_요청(브랜드_B_명);
+        브랜드_C = 브랜드_생성_요청(브랜드_C_명);
+        브랜드_D = 브랜드_생성_요청(브랜드_D_명);
+        브랜드_E = 브랜드_생성_요청(브랜드_E_명);
+        브랜드_F = 브랜드_생성_요청(브랜드_F_명);
+        브랜드_G = 브랜드_생성_요청(브랜드_G_명);
+        브랜드_H = 브랜드_생성_요청(브랜드_H_명);
+        브랜드_I = 브랜드_생성_요청(브랜드_I_명);
 
-        String 브랜드_A_ID = 브랜드_A.body().jsonPath().getString("id");
-        String 브랜드_B_ID = 브랜드_B.body().jsonPath().getString("id");
-        String 브랜드_C_ID = 브랜드_C.body().jsonPath().getString("id");
-        String 브랜드_D_ID = 브랜드_D.body().jsonPath().getString("id");
-        String 브랜드_E_ID = 브랜드_E.body().jsonPath().getString("id");
-        String 브랜드_F_ID = 브랜드_F.body().jsonPath().getString("id");
-        String 브랜드_G_ID = 브랜드_G.body().jsonPath().getString("id");
-        String 브랜드_H_ID = 브랜드_H.body().jsonPath().getString("id");
-        String 브랜드_I_ID = 브랜드_I.body().jsonPath().getString("id");
+        브랜드_A_ID = 브랜드_A.body().jsonPath().getString("id");
+        브랜드_B_ID = 브랜드_B.body().jsonPath().getString("id");
+        브랜드_C_ID = 브랜드_C.body().jsonPath().getString("id");
+        브랜드_D_ID = 브랜드_D.body().jsonPath().getString("id");
+        브랜드_E_ID = 브랜드_E.body().jsonPath().getString("id");
+        브랜드_F_ID = 브랜드_F.body().jsonPath().getString("id");
+        브랜드_G_ID = 브랜드_G.body().jsonPath().getString("id");
+        브랜드_H_ID = 브랜드_H.body().jsonPath().getString("id");
+        브랜드_I_ID = 브랜드_I.body().jsonPath().getString("id");
 
         // A 브랜드 상품 생성
         상품_생성_요청(브랜드_A_ID, Category.TOP.getKorean(), "11200");
@@ -143,9 +177,6 @@ public class PriceAnalysisAcceptanceTest extends AbstractAcceptanceTest {
     @Test
     @DisplayName("단일 브랜드로 모든 카테고리 상품을 구매할 때 최저가 상품의 카테고리 및 가격, 총액을 조회할 수 있다.")
     void findLowestBrandPrices() {
-        // given
-        샘플_데이터_생성();
-
         // when
         ExtractableResponse<Response> 최저가_조합_브랜드_상품_리스트_조회_응답 = 최저가_조합_브랜드_상품_리스트_조회_요청();
         String brandName = 최저가_조합_브랜드_상품_리스트_조회_응답.body().jsonPath().getString("brandName");
@@ -154,7 +185,7 @@ public class PriceAnalysisAcceptanceTest extends AbstractAcceptanceTest {
 
         // then
         assertThat(최저가_조합_브랜드_상품_리스트_조회_응답.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(brandName).isEqualTo("D");
+        assertThat(brandName).isEqualTo(브랜드_D_명);
         assertThat(totalPrice).isEqualTo("36,100");
 
         assertThat(categories.get(0)).containsEntry("categoryName", Category.TOP.getKorean());
@@ -176,9 +207,6 @@ public class PriceAnalysisAcceptanceTest extends AbstractAcceptanceTest {
     @Test
     @DisplayName("각 카테고리 별 최저가 상품을 조회할 수 있다.")
     void findLowestPricesBrandMix() {
-        // given
-        샘플_데이터_생성();
-
         // when
         ExtractableResponse<Response> 카테고리별_최저가_상풍_리스트_조합_조회_응답 = 카테고리별_최저가_상풍_리스트_조합_조회_요청();
         String totalPrice = 카테고리별_최저가_상풍_리스트_조합_조회_응답.body().jsonPath().getString("totalPrice");
@@ -196,14 +224,14 @@ public class PriceAnalysisAcceptanceTest extends AbstractAcceptanceTest {
         assertThat(categories.get(6)).containsEntry("categoryName", Category.SOCKS.getKorean());
         assertThat(categories.get(7)).containsEntry("categoryName", Category.ACCESSORY.getKorean());
 
-        assertThat(categories.get(0)).containsEntry("brandName", "C");
-        assertThat(categories.get(1)).containsEntry("brandName", "E");
-        assertThat(categories.get(2)).containsEntry("brandName", "D");
-        assertThat(categories.get(3)).containsEntry("brandName", "G");
-        assertThat(categories.get(4)).containsEntry("brandName", "A");
-        assertThat(categories.get(5)).containsEntry("brandName", "D");
-        assertThat(categories.get(6)).containsEntry("brandName", "I");
-        assertThat(categories.get(7)).containsEntry("brandName", "F");
+        assertThat(categories.get(0)).containsEntry("brandName", 브랜드_C_명);
+        assertThat(categories.get(1)).containsEntry("brandName", 브랜드_E_명);
+        assertThat(categories.get(2)).containsEntry("brandName", 브랜드_D_명);
+        assertThat(categories.get(3)).containsEntry("brandName", 브랜드_G_명);
+        assertThat(categories.get(4)).containsEntry("brandName", 브랜드_A_명);
+        assertThat(categories.get(5)).containsEntry("brandName", 브랜드_D_명);
+        assertThat(categories.get(6)).containsEntry("brandName", 브랜드_I_명);
+        assertThat(categories.get(7)).containsEntry("brandName", 브랜드_F_명);
 
         assertThat(categories.get(0)).containsEntry("price", "10,000");
         assertThat(categories.get(1)).containsEntry("price", "5,000");
@@ -224,9 +252,6 @@ public class PriceAnalysisAcceptanceTest extends AbstractAcceptanceTest {
     @Test
     @DisplayName("특정 카테고리의 최저가 및 최고가 상품을 조회할 수 있다.")
     void findLowestAndHighestBrandPrices() {
-        // given
-        샘플_데이터_생성();
-
         // when
         ExtractableResponse<Response> 카테고리별_최고가_및_최저가_브랜드_조회_응답 = 카테고리별_최고가_및_최저가_브랜드_조회_요청(Category.TOP.getKorean());
 
@@ -235,10 +260,31 @@ public class PriceAnalysisAcceptanceTest extends AbstractAcceptanceTest {
         List<Map<String, String>> lowestPrice = 카테고리별_최고가_및_최저가_브랜드_조회_응답.body().jsonPath().getList("lowestPrice");
         List<Map<String, String>> highestPrice = 카테고리별_최고가_및_최저가_브랜드_조회_응답.body().jsonPath().getList("highestPrice");
 
-        assertThat(lowestPrice.get(0)).containsEntry("brandName", "C");
-        assertThat(lowestPrice.get(0)).containsEntry("price","10,000");
+        assertThat(lowestPrice.getFirst()).containsEntry("brandName", 브랜드_C_명);
+        assertThat(lowestPrice.getFirst()).containsEntry("price", "10,000");
 
-        assertThat(highestPrice.get(0)).containsEntry("brandName", "I");
-        assertThat(highestPrice.get(0)).containsEntry("price", "11,400");
+        assertThat(highestPrice.getFirst()).containsEntry("brandName", 브랜드_I_명);
+        assertThat(highestPrice.getFirst()).containsEntry("price", "11,400");
+    }
+
+    @Test
+    @DisplayName("특정 카테고리의 최저가 및 최고가 상품이 추가 되어도 알맞은 상품을 조회할 수 있다.")
+    void findLowestAndHighestBrandPricesWhenAddItem() {
+        // given
+        final String 신규_상품_최저가 = "500";
+        상품_생성_요청(브랜드_G_ID, Category.TOP.getKorean(), 신규_상품_최저가);
+        // when
+        ExtractableResponse<Response> 카테고리별_최고가_및_최저가_브랜드_조회_응답 = 카테고리별_최고가_및_최저가_브랜드_조회_요청(Category.TOP.getKorean());
+
+        카테고리별_최고가_및_최저가_브랜드_조회_응답.body().jsonPath().getString("categoryName");
+
+        List<Map<String, String>> lowestPrice = 카테고리별_최고가_및_최저가_브랜드_조회_응답.body().jsonPath().getList("lowestPrice");
+        List<Map<String, String>> highestPrice = 카테고리별_최고가_및_최저가_브랜드_조회_응답.body().jsonPath().getList("highestPrice");
+
+        assertThat(lowestPrice.getFirst()).containsEntry("brandName", 브랜드_G_명);
+        assertThat(lowestPrice.getFirst()).containsEntry("price", 신규_상품_최저가);
+
+        assertThat(highestPrice.getFirst()).containsEntry("brandName", 브랜드_I_명);
+        assertThat(highestPrice.getFirst()).containsEntry("price", "11,400");
     }
 }
